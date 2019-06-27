f3d9e7fe1b17c80a89643070076b2fbcd6e678df
Cyryll Galon <galoncyryll@gmail.com>
2019-06-27 12:04:25 -0700

    changed id query to facebook id query

--- api/models/testModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const testSchema = mongoose.Schema({
-    id: String,
-    name: String,
-});
-// create Taable "test2"
-module.exports = mongoose.model('test2', testSchema);
