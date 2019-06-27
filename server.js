b3d8969b1fda2435f09bedcc587676db38a33161
Raul Sanchez <rauldevil69@gmail.com>
2019-06-26 23:42:25 -0700

    Clean Up

--- server.js
+++ server.js
@@ -1,35 +1,7 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const bodyparser = require('body-parser');
-const mongoose = require('mongoose');
-
-// reads .env files
 require('dotenv').config();
+const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 5001;
+const server = http.createServer(app);
 
-// modules
-const test = require('./controllers/test');
-
-// connect to mongoDB
-mongoose.connect(process.env.DB_PASS, { useNewUrlParser: true });
-mongoose.connection.once('open', () => {
-    console.log('connected to db');
-});
-
-const app = express();
-app.use(bodyparser.json());
-app.use(cors());
-
-// sends message
-app.get('/', (req,res) => { 
-    res.send('st API');
-});
-
-// test route for posting
-app.post('/test', (req,res) => {
-    test.testPost(req, res, mongoose);
-});
-
-app.listen(process.env.PORT || 5001, () => {
-    console.log('running on port 5001..');
-});
+server.listen(port);
